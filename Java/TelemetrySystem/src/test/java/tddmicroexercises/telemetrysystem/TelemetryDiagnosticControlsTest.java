47b8f9972f43450e0459149a91158842d3a05163
Alina Nahidi <abdul.alinakhanam@zemosolabs.com>
2023-03-08 17:38:22 +0530

    Update TelemetryDiagnosticControlsTest.java

--- Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
+++ Java/TelemetrySystem/src/test/java/tddmicroexercises/telemetrysystem/TelemetryDiagnosticControlsTest.java
@@ -7,7 +7,7 @@ import tddmicroexercises.telemetrysystem.services.TelemetryService;
 public class TelemetryDiagnosticControlsTest
 {
     
-	@Test
+    @Test
     public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
     {
         String response = "Server Response";
